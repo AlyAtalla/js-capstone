71003fd86cc9ffa94a12bae537ffd6628746fcfe
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-31 14:05:54 +0300

    Add API data file

--- src/api/getItems.js
+++
@@ -1,8 +0,0 @@
-import { ART_API, OBJECT_IDS } from '../constants.js';
-
-export const getItems = async () => Promise.all(
-  OBJECT_IDS.map(async (id) => {
-    const response = await fetch(`${ART_API}/${id}`);
-    return response.json();
-  }),
-);
